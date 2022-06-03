9be4586c7180f86fd430bc680d42143d26f1cdb9
Dom Talbot <3301714+domjtalbot@users.noreply.github.com>
2022-06-03 18:06:56 +0100

    chore: init Nx workspace (#1)

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,11 @@
+export default {
+  '*': () => 'nx format:write',
+  '{nx,workspace}.json': () => 'nx workspace-lint --fix',
+  '**/*.{js,jsx,ts,tsx}': () => 'nx affected --target=lint --fix',
+  '{apps,libs}/**/*.*': () => [
+    'nx format:write',
+    'nx affected --target=build',
+    'nx affected --target=test',
+    'nx affected --target=e2e',
+  ],
+};
