b6c59632a200964be9087d0f90f295733fecba87
Dom <3301714+domjtalbot@users.noreply.github.com>
2022-06-04 20:03:29 +0100

    chore(nx-plugin-graphql-mesh): generate `dev` executor

---
+++ libs/nx-plugin-graphql-mesh/src/executors/dev/executor.ts
@@ -0,0 +1,8 @@
+import { DevExecutorSchema } from './schema';
+
+export default async function runExecutor(options: DevExecutorSchema) {
+  console.log('Executor ran for Dev', options);
+  return {
+    success: true,
+  };
+}
