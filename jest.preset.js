c463c94212e4fcac81e659af11e5d7f78e6306f5
Dom <3301714+domjtalbot@users.noreply.github.com>
2022-06-06 21:24:08 +0100

    chore(jest): add additional coverageReporters

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,14 @@
 const nxPreset = require('@nrwl/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  coverageReporters: [
+    'clover',
+    'cobertura',
+    'html',
+    'json',
+    'lcov',
+    'text',
+    'text-summary',
+  ],
+};
