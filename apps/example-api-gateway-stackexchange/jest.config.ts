f6ea3ac78aa0f983ae61bbd117ec2076bfa77586
Dom <3301714+domjtalbot@users.noreply.github.com>
2022-07-07 22:11:27 +0100

    refactor: rename `example-api-gateway-stackexchange`

--- apps/example-api-gateway-stackexchange/jest.config.ts
+++ apps/example-api-gateway-stackexchange/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'example-api-gateway',
+  displayName: 'example-api-gateway-stackexchange',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/example-api-gateway',
+  coverageDirectory: '../../coverage/apps/example-api-gateway-stackexchange',
 };
