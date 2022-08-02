61b815ba4a3b345741e7f37c52fb778926fcd0a3
Dom <3301714+domjtalbot@users.noreply.github.com>
2022-08-02 17:44:37 +0100

    feat: generate config example api-gateways

---
+++ apps/api-gateway/javascript-wiki/cjs-config-e2e/src/integration/app.spec.ts
@@ -0,0 +1,17 @@
+describe('javascript-wiki-cjs-config', () => {
+  it('should start a mesh server', () => {
+    cy.request('/graphql/health').then((resp) => {
+      expect(resp.status).to.eq(200);
+      expect(resp.body).to.have.property('message', 'alive');
+    });
+  });
+
+  it('should still be running after 15 seconds', () => {
+    cy.wait(1000 * 15)
+      .request('/graphql/health')
+      .then((resp) => {
+        expect(resp.status).to.eq(200);
+        expect(resp.body).to.have.property('message', 'alive');
+      });
+  });
+});
