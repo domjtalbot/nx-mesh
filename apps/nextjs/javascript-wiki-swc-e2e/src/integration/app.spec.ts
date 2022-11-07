9da4ba2e1ad3e3ab7abf724b83da00f2947ded31
Dom <3301714+domjtalbot@users.noreply.github.com>
2022-11-07 20:15:24 +0000

    build: remove examples

--- apps/nextjs/javascript-wiki-swc-e2e/src/integration/app.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('nextjs-javascript-wiki', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display wiki data', () => {
-    cy.get('[data-wiki]').should('have.length.greaterThan', 0);
-  });
-});
--- libs/sdk/weatherbit/src/lib/utilities/is-error.ts
+++
@@ -1,6 +0,0 @@
-import type { Error } from '../sdk';
-
-export const isError = <T>(value: T | Error): value is Error =>
-  Object.prototype.hasOwnProperty.call(value, 'code');
-
-export default isError;
