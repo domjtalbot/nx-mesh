4b61d8718b245e1c20c654b4aedd2fc5c3719754
Dom Talbot <3301714+domjtalbot@users.noreply.github.com>
2022-11-08 12:14:00 +0000

    feat: add example stackexchange sdk

---
+++ libs/example/sdk/stackexchange/src/lib/example-sdk-stackexchange.spec.ts
@@ -0,0 +1,7 @@
+import { exampleSdkStackexchange } from './example-sdk-stackexchange';
+
+describe('exampleSdkStackexchange', () => {
+  it('should work', () => {
+    expect(exampleSdkStackexchange()).toEqual('example-sdk-stackexchange');
+  });
+});
---
+++ libs/example/sdk/stackexchange/src/lib/example-sdk-stackexchange.ts
@@ -0,0 +1,3 @@
+export function exampleSdkStackexchange(): string {
+  return 'example-sdk-stackexchange';
+}
