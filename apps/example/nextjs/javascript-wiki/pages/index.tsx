550248ee817b949922fa1151b25482b8b07d2a98
Dom <3301714+domjtalbot@users.noreply.github.com>
2022-11-08 16:04:18 +0000

    feat: add javascript-wiki nextjs example

---
+++ apps/example/nextjs/javascript-wiki/pages/index.tsx
@@ -0,0 +1,34 @@
+import type { GetStaticProps, NextPage } from 'next';
+import type { availability_queryQuery } from '@nx-mesh/example/sdk/javascript-wiki';
+
+import styles from './index.module.css';
+
+type PageData = availability_queryQuery;
+
+export const getStaticProps: GetStaticProps<PageData> = async () => {
+  const { getMeshSDK } = await import('@nx-mesh/example/sdk/javascript-wiki');
+
+  const data = await getMeshSDK().availability_query();
+
+  return {
+    props: data,
+    revalidate: 60 * 60 * 24 * 31, // 1 month
+  };
+};
+
+export const Index: NextPage<PageData> = (props) => {
+  return (
+    <div className={styles.page}>
+      <h1>JavaScript Wiki</h1>
+      <ul>
+        {props.availability?.inTheNews?.map((wiki) => (
+          <li key={wiki} data-wiki={wiki}>
+            {wiki}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Index;
