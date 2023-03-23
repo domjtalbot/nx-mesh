f37e2f9423c3125a48c2270c72befac2ccb5f6de
Dom <3301714+domjtalbot@users.noreply.github.com>
2023-03-23 21:20:38 +0000

    build: fix workspace errors

--- packages/nx-mesh/jest.config.ts
+++ packages/nx-mesh/jest.config.ts
@@ -1,14 +1,26 @@
 /* eslint-disable */
+import { readFileSync } from 'fs';
+
+const swcrc = JSON.parse(readFileSync(`${__dirname}/.lib.swcrc`, 'utf-8'));
+
+// Reading the SWC compilation config and remove the "exclude"
+// for the test files to be compiled by SWC
+const swcJestConfig = {
+  ...swcrc,
+  exclude: undefined,
+  jsc: {
+    ...swcrc.jsc,
+    experimental: {
+      plugins: [['jest_workaround', {}]],
+    },
+  },
+};
+
 export default {
   displayName: 'nx-mesh',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/packages/nx-mesh',
